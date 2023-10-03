0609f40b0489b8a351652ad5c06517d1fabcba09
johanvadenne <johanvadenne@gmail.com>
2023-10-03 20:26:38 +0200

    1.0.1.0

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
 const urlBlague = "https://v2.jokeapi.dev/joke/Any?lang=fr"
-const element = document.getElementById("blague")
-const tableau = document.getElementById("corpTableauBlague")
 let numBlague = 0;
+let reponseBlague = [];
 
 // FR: fait une requette GET à une API
 // EN: makes a GET request to an API
@@ -22,16 +21,31 @@ function appelleAPI(url) {
 function uneBlague() {
     appelleAPI(urlBlague)
         .then(blague => {
+            const tableau = document.getElementById("corpTableauBlague")
             console.log(blague);
             numBlague++;
             let ligne = "";
-            element.innerHTML = blague.setup + "<br>" + blague.delivery;
             ligne += "<tr>";
             ligne += "<td>" + numBlague + "</td>";
             ligne += "<td>" + blague.setup + "</td>";
-            ligne += "<td>" + blague.setup + "</td>";
+            ligne += "<td id=\"blague"+numBlague+"\">" + blague.delivery + "</td>";
+            ligne += "<td><input id=\"checkbox"+numBlague+"\" onclick=\"cacher("+numBlague+")\" type=\"checkbox\"></input></td>";
             ligne += "</tr>";
 
-            tableau.innerHTML += ligne
+            tableau.innerHTML = ligne + tableau.innerHTML
+            const ligneBlague = document.getElementById("blague"+numBlague);
+            ligneBlague.innerHTML = "*".repeat(20);
+            reponseBlague.push(blague.delivery);
         });
+}
+
+function cacher(idBlague) {
+    const ligneBlague = document.getElementById("blague"+idBlague);
+    const lignecheckbox = document.getElementById("checkbox"+idBlague);
+    if (lignecheckbox.checked) {
+        ligneBlague.innerHTML = reponseBlague[idBlague-1];
+    }
+    else {
+        ligneBlague.innerHTML = "*".repeat(20);
+    }
 }
